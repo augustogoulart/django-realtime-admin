cbf2c64c078c20eee13bad775ea2172edad1fd00
Augusto <augusto@britecore.com>
2020-04-26 20:05:20 -0300

    Add initial model definition

--- realtime_admin/core/models.py
+++ realtime_admin/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    product = models.CharField(max_length=255)
+    client = models.CharField(max_length=255)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return self.product
