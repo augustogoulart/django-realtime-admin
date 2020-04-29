0d4e796f6c570d93f0a8b230c223f51e0621ab65
Augusto <augusto@britecore.com>
2020-04-28 23:33:27 -0300

    Add extra javascript to change_list page

--- realtime_admin/settings.py
+++ realtime_admin/settings.py
@@ -16,7 +16,6 @@ from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'realtime_admin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
