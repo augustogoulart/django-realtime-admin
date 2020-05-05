7921b1af7967b53c6db60ba6e4dcd783be1caade
Augusto <augusto@britecore.com>
2020-05-05 00:16:44 -0300

    New row shoud be added to the top

--- realtime_admin/core/static/core/ws.js
+++ realtime_admin/core/static/core/ws.js
@@ -1,14 +1,30 @@
-const ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-const ws = new WebSocket(
-    ws_scheme
-    + '://'
-    + window.location.host
-    + '/ws/list/'
-);
-ws.onmessage = function (e) {
-    return
-};
+(function ($) {
+    const ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+    const ws = new WebSocket(
+        ws_scheme
+        + '://'
+        + window.location.host
+        + '/ws/list/'
+    );
+    ws.onmessage = function (e) {
+        const data = JSON.parse(e.data)
 
-ws.onclose = function (e) {
-    console.error('Chat socket closed unexpectedly');
-};
+        let newRow =
+            `<tr class="row2">
+               <td class="action-checkbox">
+                 <input type="checkbox" name="_selected_action" value="1" class="action-select">
+               </td>
+               <th class="field-product">
+                 <a href="/admin/core/order/1/change/">${data.product}</a>
+               </th>
+               <td class="field-client">${data.client}</td>
+               <td class="field-value">${data.value}</td>
+            </tr>`
+
+        $("#result_list tbody").prepend(newRow)
+    };
+
+    ws.onclose = function (e) {
+        console.error('Chat socket closed unexpectedly');
+    };
+})(django.jQuery);
